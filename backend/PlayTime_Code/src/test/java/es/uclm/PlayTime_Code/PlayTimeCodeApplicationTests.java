d71bfcc00355900dc154c62e8144d8867dcdfdb5
Pedroles04 <pedromartingonzalez4@gmail.com>
2026-04-08 23:56:03 +0200

    feat: estructura base del backend con Spring Boot y H2
    Closes #2

---
+++ backend/PlayTime_Code/src/test/java/es/uclm/PlayTime_Code/PlayTimeCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uclm.PlayTime_Code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayTimeCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
